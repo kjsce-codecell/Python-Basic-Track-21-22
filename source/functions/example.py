56fb178b154d25c0192c12a4adeced9cdfba54c0
Manasi Varaiya <m.varaiya@somaiya.edu>
2021-09-21 20:35:54 +0530

    parameters done

--- source/functions/example.py
+++ source/functions/example.py
@@ -4,3 +4,21 @@ def my_name():
 
 #calling function
 my_name()
+
+#with one argument/data
+def my_name(fname):
+  print(fname + " Varaiya")
+
+my_name("Manasi")  #function calling 
+
+#with two arguments/data
+def my_name(fname,lname):
+  print(fname +" "+ lname)
+
+my_name("Manasi","Varaiya") #function calling 
+
+#This Code will give an error
+def my_name(fname,lname):
+  print(fname +" "+ lname)
+
+my_name("Manasi") #function calling 
