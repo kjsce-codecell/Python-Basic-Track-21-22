c1cdd6063c049f251939c324a351691c5f1f124c
Manasi Varaiya <m.varaiya@somaiya.edu>
2021-09-21 20:52:50 +0530

    multiplication function and pass keyword done

--- source/functions/example.py
+++ source/functions/example.py
@@ -15,10 +15,26 @@ my_name("Manasi")  #function calling
 def my_name(fname,lname):
   print(fname +" "+ lname)
 
-my_name("Manasi","Varaiya") #function calling 
+my_name("Manasi","Varaiya")        #function calling 
 
 #This Code will give an error
 def my_name(fname,lname):
   print(fname +" "+ lname)
 
-my_name("Manasi") #function calling 
+my_name("Manasi")       #function calling 
+
+# function to show multiplication
+def multi(x):         #creating the function
+  return 2 * x
+
+print(multi(2))         #function calling 
+print(multi(9))         #function calling 
+
+#usage of pass keyword
+def myfunction():
+  pass
+
+# having an empty function definition like this, would raise an error without the pass statement
+
+def myfunction():
+    
