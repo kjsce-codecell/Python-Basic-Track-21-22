b702b39856fb305ac1b37600e6cfe61222c27dde
SahilSayani <sahilsayani7@gmail.com>
2021-09-23 18:23:54 +0530

    tuple.py added

---
+++ source/ds/tuple.py
@@ -0,0 +1,84 @@
+'''
+# Empty tuple
+my_tuple = ()
+print(my_tuple)
+
+my_tuple = 3, 4.6, "dog"
+print(my_tuple)
+
+# Tuple having integers
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+# tuple with mixed datatypes
+my_tuple = (1, "Hello", 3.4)
+print(my_tuple)
+
+# nested tuple
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+print(my_tuple)
+
+#tuple with one element???
+my_tuple = ("hello")
+print(type(my_tuple))  # <class 'str'>
+
+# Creating a tuple having one element
+my_tuple = ("hello",)
+print(type(my_tuple)) 
+'''
+
+'''
+# Accessing tuple elements using indexing
+my_tuple = ('p','e','r','m','i','t')
+
+print(my_tuple[0])   # 'p' 
+print(my_tuple[5])   # 't'
+
+
+# nested tuple
+n_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+
+# nested index
+print(n_tuple[0][3])       # 's'
+print(n_tuple[1][1])       # 4
+print(n_tuple[-1])
+print(n_tuple[1:3])        #slicing works in the same fashion as that of lists
+'''
+
+
+'''
+# Changing tuple values
+my_tuple = (4, 2, 3, [6, 5])
+
+
+# TypeError: 'tuple' object does not support item assignment
+# my_tuple[1] = 9
+
+# However, item of mutable element can be changed
+my_tuple[3][0] = 9    # Output: (4, 2, 3, [9, 5])
+print(my_tuple)
+'''
+
+'''
+# Deleting tuples
+my_tuple = ('p', 'r', 'o', 'g', 'r', 'a', 'm')
+
+# can't delete items
+# TypeError: 'tuple' object doesn't support item deletion
+# del my_tuple[3]
+
+# Can delete an entire tuple
+del my_tuple
+
+# NameError: name 'my_tuple' is not defined
+print(my_tuple)
+'''
+
+'''
+#tuple methods
+my_tuple = ('a', 'p', 'p', 'l', 'e',)
+
+print(my_tuple.count('p'))  # Output: 2
+print(my_tuple.index('l'))  # Output: 3
+
+'''
