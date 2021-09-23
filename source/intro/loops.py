b6a72b0e7b122bd59975163538222e109440d4e9
Manasi Varaiya <m.varaiya@somaiya.edu>
2021-09-23 21:00:06 +0530

    star sequence

--- source/intro/loops.py
+++ source/intro/loops.py
@@ -3,8 +3,13 @@ counting using for loops
 '''
 
 
-for i in 'Codecell':
-    print(i)
+# for i in 'Codecell':
+#     print(i)
 
-for j in range(0, 5):
-    print(j)
+# for j in range(0, 5):
+#     print(j)
+
+for i in range(0, 3):
+    for j in range(0, i+1):
+        print("* ",end="")
+    print("\r")
