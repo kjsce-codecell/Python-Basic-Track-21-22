beec8b20087e61d6241b86d875a829527b505377
Manasi Varaiya <m.varaiya@somaiya.edu>
2021-09-24 16:26:41 +0530

    User authentication program

--- source/Programs/User authentication.py
+++ source/Programs/User authentication.py
@@ -1,1 +1,18 @@
-d = {}
+d = {"Manasi": "1234",
+  "Hussain": "0000",
+  "Sahil": "6789",
+  "Soumen": "0202"}
+print("Enter username and password") 
+while(True):
+    username=input("Enter username: ") 
+    if username in d.keys():
+        print("Welcome " + username + "!") 
+        # while(True):
+        password=input("Enter password: ") 
+        if(d[username]==password):
+            print("Valid password")
+            print("You have been logged in!\n") 
+        else:
+            print("Wrong password!\nRe-enter password.")
+    else:
+        print("Username does not exist") 
