21b09936a38a01adf69768b332795cc009ee2661
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-09-25 20:14:23 +0530

    comments added in say0.py

--- source/speech/say0.py
+++ source/speech/say0.py
@@ -4,8 +4,13 @@ https://github.com/nateshmbhat/pyttsx3
 
 import pyttsx3
 
+# to initialize the library
+# why? because thats just how the programmer designed it
 engine = pyttsx3.init()
 
-engine.say("Order homemade Food Online. Only on Chef Cities dot com")
+# I can use this engine to say things
+engine.say("Chef Cities dot com is a great website!")
+
+# then I will run this engine and wait for it before my program quits
 engine.runAndWait()
 
